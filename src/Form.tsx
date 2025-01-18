94e7c5701dc155c786b92c9791457c307de5552f
Tanveer Singh Taneja <tanveer.singh926@gmail.com>
2025-01-17 19:17:26 -0800

    Add form components and validation schema for user input.

--- src/Form.tsx
+++ src/Form.tsx
@@ -1,17 +1,3 @@
-import { useForm } from 'react-hook-form'
-import { zodResolver } from '@hookform/resolvers/zod'
-
-import { formSchema } from './schema'
-
 export const Form = () => {
-  const formMethods = useForm({
-    resolver: zodResolver(formSchema),
-  })
-
-  // The callback to use when the form is submitted
-  const saveData = (data) => {
-    console.log(data)
-  }
-
-  return <div className="Form">Your exercise code goes here</div>
+  return <div>Hi</div>
 }
